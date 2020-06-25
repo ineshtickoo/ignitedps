a6cd619bb0de3e33fbb6cebb121035f751a719e9
unknown <inesh.tickoo@gmail.com>
2020-06-25 13:15:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/epsilon", (req, res) => {
+  res.sendFile(path.join(__dirname, "./epsilon.html"));
+});
+
+app.get("/epsilon/prompt", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Epsilon2020IntraPrompt.html"));
+});
+
+app.get("/epsilon/guidelines", (req, res) => {
+  res.sendFile(path.join(__dirname, "./epsilonguidelines.html"));
+});
+
+const port = 8080;
+app.listen(port || process.env.PORT, () => {
+  console.log(`App running on ${port}`);
+});
